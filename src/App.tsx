3d433de80ced24f18fc5664300c07e645034252b
HuySG <huybc2011@gmail.com>
2024-05-21 17:15:54 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route, Router } from "react-router-dom";
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Toaster } from "@/components/ui/toaster";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes là nơi mọi người có thể thấy như việc đăng kí và đăng nhập */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        {/* private routes là nơi mình có thể thấy khi đăng nhập vào  */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
+  );
+};
+
+export default App;
